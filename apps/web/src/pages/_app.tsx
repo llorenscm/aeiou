7b4a8732b85fc62400f11d678ec193284185c033
Llorenç Comellas <llorenscomellas@gmail.com>
2022-11-21 18:03:51 +0100

    update favicon

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -11,11 +11,12 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width" />
         <meta name="description" content="App per millorar el traç" />
         <meta name="theme-color" content="#aee8da" />
+        <meta name="msapplication-TileColor" content="#aee8da" />
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
         <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5"></link>
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#aee8da"></link>
         <link rel="preload" href="/fonts/Pompiere-Regular.ttf" as="font" type="font/ttf"></link>
         <link rel="preload" href="/fonts/Massallera-Regular.ttf" as="font" type="font/ttf"></link>
       </Head>
