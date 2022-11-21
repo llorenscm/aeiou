ddaeddfa117f5267e2c6e4061c539248dce57d22
Llorenç Comellas <llorenscomellas@gmail.com>
2022-11-21 16:09:54 +0100

    change path favicon

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -14,8 +14,8 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"></link>
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5"></link>
         <link rel="preload" href="/fonts/Pompiere-Regular.ttf" as="font" type="font/ttf"></link>
         <link rel="preload" href="/fonts/Massallera-Regular.ttf" as="font" type="font/ttf"></link>
       </Head>
