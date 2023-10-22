9f961cff6aaf14559b5b594f643ef6abee1c0913
Llorenç Comellas <llorenscomellas@gmail.com>
2023-10-22 19:19:30 +0200

    ajustes 404

--- apps/web/src/pages/404.tsx
+++ apps/web/src/pages/404.tsx
@@ -8,7 +8,7 @@ export default function Custom404() {
     <div>
       <main className={styles.main}>
         <h1>No trobem la pàgina que busques</h1>
-        <h3>Pots seguir utilitzant la nostra aplicació tornant a l'inici</h3>
+        <h3>Fes clic al botó inici per continuar utilitzant la nostra aplicació.</h3>
         <Button mode="secondary" href="/" size="xxl" label="home">
           <Icon icon="home" size="xl" />
         </Button>
