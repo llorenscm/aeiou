321764ee397020786b71fcc0b52d18480eed150f
Llorenç Comellas <llorenscomellas@gmail.com>
2023-10-22 18:48:14 +0200

    ajustes errores

--- apps/web/src/sections/shared/useData.ts
+++ apps/web/src/sections/shared/useData.ts
@@ -1,32 +1,34 @@
+import * as icons from "@aeiou/design-system/src/icons/icons";
+
 import aeiou from "../../data/aeiou.json";
+
+export type Icons = keyof typeof icons;
 
 export interface Data {
   text: string;
-  imageSrc?: string;
-  icon?: string;
+  imageSrc: string;
   slug?: string;
-  paraules?: [];
-  letters?: [];
-  numbers?: [];
+  icon: Icons;
+  paraules?: [{ text: string; icon: string }];
 }
 
-export function useLetters(): any {
+export function useLetters(): Data[] {
   const letters = aeiou[0]["letters"];
 
-  return letters;
+  return letters as [];
 }
-export function useNumbers(): any {
+export function useNumbers(): Data[] {
   const numbers = aeiou[1]["numbers"];
 
-  return numbers;
+  return numbers as [];
 }
 
-export function useWords(slug: string): any {
+export function useWords(slug: string): Data[] {
   const wordsStartWithLetter = aeiou[0]["letters"].filter((obj) => {
     return obj.slug === slug;
   });
 
   const words = wordsStartWithLetter[0]["paraules"];
 
-  return words;
+  return words as [];
 }
